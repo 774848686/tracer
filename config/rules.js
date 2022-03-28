a19cc391e24ea1708bba0a3f436c34efce640500
774848686 <774848686@qq.com>
2022-03-28 10:25:33 +0800

    打包js转es5语法

--- config/rules.js
+++ config/rules.js
@@ -1,19 +1,41 @@
 module.exports = [
-    // JavaScript: Use Babel to transpile JavaScript files
-    {
-      test: /\.js$/,
-      use: ['babel-loader']
-    },
+  // JavaScript: Use Babel to transpile JavaScript files
+  {
+    test: /\.js$/,
+    exclude: /node_modules/,
+    use: {
+      loader: 'babel-loader',
+      options: {
+        presets: [
+          ['@babel/preset-env',
+            // 使用promise 更高级语法
+            {
+              useBuiltIns: 'usage',
+              "modules": false,
+              corejs: 3,
+              targets: {
+                chrome: '58',
+                ie: '9',
+                firefox: '60',
+                safari: '10',
+                edge: '17'
+              }
+            }
+          ]
+        ]
+      }
+    }
+  },
 
-    // Images: Copy image files to build folder
-    {
-      test: /\.(?:ico|gif|png|jpg|jpeg)$/i,
-      type: 'asset/resource'
-    },
+  // Images: Copy image files to build folder
+  {
+    test: /\.(?:ico|gif|png|jpg|jpeg)$/i,
+    type: 'asset/resource'
+  },
 
-    // Fonts and SVGs: Inline files
-    {
-      test: /\.(woff(2)?|eot|ttf|otf|svg|)$/,
-      type: 'asset/inline'
-    },
-  ]
+  // Fonts and SVGs: Inline files
+  {
+    test: /\.(woff(2)?|eot|ttf|otf|svg|)$/,
+    type: 'asset/inline'
+  },
+]
--- config/webpack.prod.js
+++ config/webpack.prod.js
@@ -30,7 +30,7 @@ module.exports = merge(common, {
           'postcss-loader',
           'sass-loader',
         ],
-      },
+      }
     ],
   },
   plugins: [
